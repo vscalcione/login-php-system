c4e54f354c123c2c4f2447e6bd625969e59c8861
vscalcione <vincenzo99.scalcione@gmail.com>
2020-03-14 21:00:45 +0100

    Upload profile picture

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-$(document).ready(function(e){
+$(document).ready( function(e){
+    let $uploadFile = $('#register .upload-profile-image input[type="file"]');
+    $uploadFile.change(function(){
+        readURL(this);
+    });
+
    $('#registration-form').submit(function(event){
        let $password = $('#password');
        let $confirm = $('#confirmPassword');
@@ -7,7 +12,16 @@ $(document).ready(function(e){
            return true;
        }else{
            $error.text("Le password devono essere uguali");
-           event.preventDefault();
+           // event.preventDefault();
        }
    })
 });
+
+function readURL(input){
+   if(input.files && input.files[0]){
+       let reader = new FileReader();
+       reader.onload = function(e){
+           $('#register .upload-profile-image .img').attr('src', e.target.result);
+       }, reader.readAsDataURL(input.files[0]);
+   }
+};
