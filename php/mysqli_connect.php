e0c82af6399db9c484a9211ce41f52d2d4f7e906
vscalcione <vincenzo99.scalcione@gmail.com>
2020-03-14 23:52:36 +0100

    Add initial connection on MariaDB Database

--- php/mysqli_connect.php
+++ php/mysqli_connect.php
@@ -8,6 +8,12 @@ define('DB_HOST', 'localhost');
 try{
     //connection variable
     $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    //encoded language
+    mysqli_set_charset($connection, 'utf8');
 }catch(Exception $exception){
+    print "An Exception occured. Message ".$exception -> getMessage();
+}catch(Error $error){
+    print "The system is busy. Please, try later";
 }
 
