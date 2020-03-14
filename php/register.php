41dc26fd192cdc39289faa3031520a513feebc32
vscalcione <vincenzo99.scalcione@gmail.com>
2020-03-14 16:32:52 +0100

    Add images and register page structure

--- php/register.php
+++ php/register.php
@@ -4,7 +4,23 @@
 
 <!-- Start Registration area -->
  <section id="register">
-     <p>Hello world</p>
+     <div class="row m-0">
+         <div class="col-lg-4 offset-lg-2">
+             <div class="text-center pb-5">
+                 <h1 class="login-title text-dark">Register</h1>
+                 <p class="p-1 m-0 font-ubuntu text-black-50">Register and enjoy additional features</p>
+                 <span> I already have <a href="login.php">Login</a></span>
+             </div>
+             <div class="upload-profile-image">
+                 <div class="text-center">
+                     <div class="d-flex justify-content-center">
+                         <img class="camera-icon" src="../assets/camera-solid.svg" alt="profile-photo" />
+                     </div>
+                     <img src="../assets/profile/beard.png" alt="profile" />
+                 </div>
+             </div>
+         </div>
+     </div>
  </section>
 
 <?php
