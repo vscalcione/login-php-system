b7c2db7298e2069e2246e4d79298507dc930b65e
vscalcione <vincenzo99.scalcione@gmail.com>
2020-03-15 13:11:26 +0100

    Add simple php documentation in 'register-process.php'

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 This is a simple login system made in PHP and MySQL Database.
 
 For run this project, you can download XAMPP Software on your machine and then
-put the project's folder in xamppInstallationFolder/htdocs
+put the project's folder in <b>xampp_installation_folder/htdocs</b>
 <br>
-<li>If you use Windows is: <b>C:/xampp/htdocs/projectFolder</b></li>
-<li>If you use Linux is: <b>/opt/lampp/htdocs</b></li>
+<li>If you use Windows is: <b>C:/xampp/htdocs/login-php-system</b></li>
+<li>If you use Linux is: <b>/opt/lampp/htdocs/login-php-system</b></li>
 <br>
 After run XAMPP with a click on the software and click on <b>Apache Web server start</b>. Then open browser and put on bar the link
-<em><b> http://localhost:80/projectFolder</b></em>
+<em><b> http://localhost/login-php-system/php/register.php</b></em>
+
+##
+<li>If you use Windows, simply click on XAMPP icon and click on startup for MySQL and Apache Web Server</li>
+<li>If you use Linux, open terminal with shortcut <code>Ctrl + Alt + T</code>
+and type command <code>$ sudo /opt/lampp/lampp start</code>.
--- php/register-process.php
+++ php/register-process.php
@@ -1,35 +1,44 @@
 <?php
 
+    /* The help.php file is included which contains the two functions for validating
+       the fields entered by the user (text, email)
+    */
     require('helper.php');
 
     //error variable
     $error = array();
 
+    /* First name field validation with a error message if the field is empty */
     $firstName = validate_input_text($_POST['firstName']);
     if(empty($firstName)){
         $error[] = "Hai dimenticato di inserire il tuo nome";
     }
 
+    /* Last name field validation with a error message if the field is empty */
     $lastName = validate_input_text($_POST['lastName']);
     if(empty($lastName)){
         $error[] = "Hai dimenticato di inserire il tuo cognome";
     }
 
+    /* Born date field validation with a error message if the field is empty */
     $bornDate = validate_input_text($_POST['dataNascita']);
     if(empty($bornDate)){
         $error[] = "Hai dimenticato di inserire la tua data di nascita";
     }
 
+    /* Email field validation with a error message if the field is empty */
     $email = validate_input_email($_POST['email']);
     if(empty($email)){
         $error[] = "Hai dimenticato di inserire la tua E-mail";
     }
 
+    /* Password field validation with a error message if the field is empty */
     $password = validate_input_email($_POST['password']);
     if(empty($password)){
         $error[] = "Hai dimenticato di inserire password";
     }
 
+    /* Confirm password field validation with a error message if the field is empty */
     $confirmPassword = validate_input_email($_POST['confirmPassword']);
     if(empty($confirmPassword)){
         $error[] = "Hai dimenticato di inserire la password di conferma";
@@ -41,10 +50,9 @@
         require('mysqli_connect.php');
 
         //make a query
-        $query = "INSERT INTO user(user_id, first_name, last_name, email, password, profile_image, register_date)";
-        $query .= "VALUES ('', ?, ?, ?, ?, ?, NOW())";
-
+        $query = "INSERT INTO user(user_id, first_name, last_name, email, password, profile_image, register_date) VALUES ('', ?, ?, ?, ?, ?, NOW())";
         $variable = mysqli_stmt_init($connection);
+
     }else{
         echo 'not validate';
     }
